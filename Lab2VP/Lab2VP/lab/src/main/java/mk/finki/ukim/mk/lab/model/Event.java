39db62a02333b9237cf890176c00ae1740d1ad64
ruleva <139155207+ruleva@users.noreply.github.com>
2024-12-06 18:23:06 +0100

    first commit

---
+++ Lab2VP/Lab2VP/lab/src/main/java/mk/finki/ukim/mk/lab/model/Event.java
@@ -0,0 +1,31 @@
+package mk.finki.ukim.mk.lab.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private String name;
+    private String description;
+    private double popularityScore;
+    @ManyToOne
+    @JoinColumn(name = "location_id", nullable = false)
+    private Location location;
+
+    public Event(String theaterShow, String s, double v,Location location) {
+        this.id = (long) (Math.random() * 1000);
+        this.name = theaterShow;
+        this.description = s;
+        this.popularityScore = v;
+        this.location = location;
+    }
+}
